80afdcd0e5134268713be933c869a1c6be268a3b
MehmetAliAkar <maliakar123@gmail.com>
2025-02-24 02:19:31 +0300

    UI Added

---
+++ Frontends/ShoppingCart.WebUI/ViewComponents/UILayoutViewComponents/_TopBarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _TopBarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
